12a3eebcc54b7790632908cdb992a153a1146a06
wahyusinggihw <wahyusinggih2402@gmail.com>
2022-12-25 18:34:14 +0700

    revisi tempat konten ke layout

--- views/components/sidebar.php
+++ views/components/sidebar.php
@@ -67,28 +67,3 @@
    </ul>
 
  </aside><!-- End Sidebar-->
-
- <main id="main" class="main">
-
-   <div class="pagetitle">
-     <h1>Dashboard</h1>
-     <nav>
-       <ol class="breadcrumb">
-         <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-         <li class="breadcrumb-item active">Dashboard</li>
-       </ol>
-     </nav>
-   </div><!-- End Page Title -->
-
-   <!-- KONTEN DARI SIDEBAR -->
-   <section class="section dashboard">
-     <div class="row">
-
-       <?php
-        include($childView);
-        ?>
-
-     </div>
-   </section>
-
- </main><!-- End #main -->
--- views/layouts/app.php
+++ views/layouts/app.php
@@ -38,7 +38,33 @@
     <?php
     include('views/components/header.php');
     include('views/components/sidebar.php');
+
     ?>
+    <!-- KONTEN DARI SIDEBAR -->
+    <main id="main" class="main">
+
+        <div class="pagetitle">
+            <h1>Dashboard</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item active">Dashboard</li>
+                </ol>
+            </nav>
+        </div><!-- End Page Title -->
+
+        <!-- KONTEN DARI SIDEBAR -->
+        <section class="section dashboard">
+            <div class="row">
+
+                <?php
+                include($childView);
+                ?>
+
+            </div>
+        </section>
+
+    </main><!-- End #main -->
 
     <!-- <script src="public/js/script.js"></script> -->
 
